66fe4087844ec26057ee24327ce2334623c73539
Michal Luczak <michal.luczak@applover.pl>
2018-02-03 05:33:25 +0100

    fix assets uniqueness check

--- app/controllers/api/v1/chain_assets_controller.rb
+++ app/controllers/api/v1/chain_assets_controller.rb
@@ -31,16 +31,32 @@ class Api::V1::ChainAssetsController < Api::V1::ApiController
     chain_asset = ChainAsset.find_by(alias: params[:alias])
     signer = Chain::HSMSigner.new
     user = User.find_by(username: params[:username])
+    return duplicate_approval unless validate_single_approval(user, current_user)
     chain = Chain::Client.new(access_token: Rails.application.secrets.chain_token,
                               url: Rails.application.secrets.chain_route)
     signer.add_key(chain_asset.keys[0], chain.mock_hsm.signer_conn)
     signer.add_key(chain_asset.keys[1], chain.mock_hsm.signer_conn)
     tx = chain.transactions.build do |b|
       b.issue asset_alias: params[:alias], amount: 1
-      b.control_with_account account_alias: user.username, asset_alias: params[:alias], amount: 1
+      b.control_with_account account_alias: current_user.username, asset_alias: params[:alias], amount: 1
     end
+
     signed_tx = signer.sign(tx)
     chain.transactions.submit(signed_tx)
+
+    signer = Chain::HSMSigner.new
+    signer.add_key(chain_asset.keys[0], chain.mock_hsm.signer_conn)
+    signer.add_key(chain_asset.keys[1], chain.mock_hsm.signer_conn)
+    signer.add_key(user.chain_key, chain.mock_hsm.signer_conn)
+    signer.add_key(current_user.chain_key, chain.mock_hsm.signer_conn)
+    payment = chain.transactions.build do |b|
+      b.spend_from_account account_alias: current_user.username, asset_alias: params[:alias], amount: 1
+      b.control_with_account account_alias: user.username, asset_alias: params[:alias], amount: 1
+    end
+
+    signed_payment = signer.sign(payment)
+
+    chain.transactions.submit(signed_payment)
     head :created
   end
 
@@ -49,12 +65,13 @@ class Api::V1::ChainAssetsController < Api::V1::ApiController
   def asset_params
     params.permit(:name, :alias, :value)
   end
+
   def check_uniqueness(name, value)
     vals = []
     chain = Chain::Client.new(access_token: Rails.application.secrets.chain_token,
                               url: Rails.application.secrets.chain_route)
-    balances = chain.balances.query(filter: "asset_definition.facebook_id=$1",
-                                    filter_params: ["2134123412341234"])
+    balances = chain.balances.query(filter: "asset_definition.#{name}=$1",
+                                    filter_params: [value])
     balances.each { |b|
       b.sum_by['asset_alias']
       vals << b.amount
@@ -62,7 +79,24 @@ class Api::V1::ChainAssetsController < Api::V1::ApiController
     return true if vals.empty?
   end
 
+  def validate_single_approval(user, current_user)
+    vals = []
+    chain = Chain::Client.new(access_token: Rails.application.secrets.chain_token,
+                              url: Rails.application.secrets.chain_route)
+    txs = chain.transactions.query(
+        filter: 'inputs(account_alias=$1) OR outputs(account_alias=$2)',
+        filter_params: [current_user.username, user.username],
+    ).each do |tx|
+      vals << tx.id
+    end
+    return true if vals.empty?
+  end
+
   def not_unique
     render json: { errors: ["#{name} is already taken"] }, status: :unprocessable_entity
+  end
+
+  def duplicate_approval
+    render json: { errors: ["Approval between those users already occured"] }, status: :unprocessable_entity
   end
 end
